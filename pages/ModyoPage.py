873203632a3207169f966c8f0bf0e4ebbd6892d5
Minsait_Hector Isaac Caldera Cardenas <NEM.hectorc@coppel.com>
2025-02-18 18:22:43 -0600

    f

--- pages/ModyoPage.py
+++ pages/ModyoPage.py
@@ -8,10 +8,10 @@ class ProductListPage(ElementsInteractions):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-    xpath_products = "//span[contains(text(), 'Products')]"
+    xpath_welcome = "//h2[contains(text(), 'Bienvenido a Modyo')]"
 
     def verify_active_session(self):
-        if self.is_element_displayed(self.xpath_products, "xpath"):
+        if self.is_element_displayed(self.xpath_welcome, "xpath"):
             self.log.info("Estamos en la pagina de login")
         else:
             self.log.error("No estamos en la pagina de Login")
